0d078d7bcfdb3929ee54e40bcf1097fab401cfdc
thetego <30575376+thetego@users.noreply.github.com>
2020-06-11 22:09:25 +0300

    asd

---
+++ hypershit/Assets/TimeController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeController : MonoBehaviour
+{
+    public GameObject[] Blocks;
+    public GameObject player;
+	[Range(0, 10)]
+	public float speed;
+
+    private void Update() {
+
+
+		player.transform.Translate(0, -speed * Time.deltaTime, 0);
+
+        foreach(GameObject block in Blocks)
+        {
+            if (Vector3.Distance(block.transform.position,player.transform.position)<=2)
+            {
+				speed = 1;
+				print(block);
+				Destroy(block);
+            }
+			else if (Vector3.Distance(block.transform.position, player.transform.position) > 2)
+			{
+				speed = 2.5f;
+			}
+
+        }
+    }
+
+}
