4eed81e93f490d807a4cff8e39f7ee65a999ccc6
thetego <30575376+thetego@users.noreply.github.com>
2020-06-12 23:22:27 +0300

    complete project

---
+++ hypershit/Assets/Finish.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Finish : MonoBehaviour
+{
+	TimeController tc;
+
+	private void Awake()
+	{
+		tc = Camera.main.GetComponent<TimeController>();
+	}
+	private void OnTriggerEnter (Collider collision)
+	{
+		if (collision.transform.tag == "End")
+		{
+			tc.retry = true;
+			tc.speed = 0;
+		}
+	}
+}
--- hypershit/Assets/TimeController.cs
+++ hypershit/Assets/TimeController.cs
@@ -4,30 +4,50 @@ using UnityEngine;
 
 public class TimeController : MonoBehaviour
 {
-    public GameObject[] Blocks;
-    public GameObject player;
+	public GameObject[] Blocks;
+	public GameObject player;
 	[Range(0, 10)]
 	public float speed;
+	public bool clicked, trigger, retry;
+	Portal portal;
 
-    private void Update() {
+	private void Awake()
+	{
+		portal = GetComponent<Portal>();
+	}
 
-
+	private void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.R))
+		{
+			Application.LoadLevel(0);
+		}
 		player.transform.Translate(0, -speed * Time.deltaTime, 0);
 
-        foreach(GameObject block in Blocks)
-        {
-            if (Vector3.Distance(block.transform.position,player.transform.position)<=2)
-            {
-				speed = 1;
-				print(block);
-				Destroy(block);
-            }
-			else if (Vector3.Distance(block.transform.position, player.transform.position) > 2)
+		if (Input.GetMouseButton(0))
+		{
+			speed = 1;
+			clicked = true;
+			if (!trigger)
+			StartCoroutine("Wait");
+		}
+		else
+		{
+			if (!retry)
 			{
-				speed = 2.5f;
+				speed = 2;
 			}
+			clicked = false;
+			if (trigger)
+			portal.CreatePortal(); trigger = false;
+		}
+	}
 
-        }
-    }
+	IEnumerator Wait()
+	{
+		portal.line.transform.position = new Vector3(portal.line.transform.position.x, player.transform.position.y, portal.line.transform.position.z);
+		trigger = true;
+		yield return null;
+	}
 
 }
