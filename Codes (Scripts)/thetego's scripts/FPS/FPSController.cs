a0fd04aa26e38df931da1ca29bc9cb17afdec9ba
thetego <30575376+thetego@users.noreply.github.com>
2020-09-04 23:36:15 +0300

    FPS

---
+++ Codes (Scripts)/thetego's scripts/FPS/FPSController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPSController : MonoBehaviour
+{
+	public float speed = 15f;
+
+	public CharacterController cc;
+	public float gravity ;
+
+	Vector3 vec;
+
+
+	private void Update()
+	{
+		float h = Input.GetAxis("Horizontal");
+		float v = Input.GetAxis("Vertical");
+
+		Vector3 Move = transform.right * h + transform.forward * v;
+
+		cc.Move(Move * speed * Time.deltaTime);
+
+		vec.y += gravity * Time.deltaTime;
+
+		cc.Move(vec * Time.deltaTime);
+
+
+	}
+
+}
---
+++ Codes (Scripts)/thetego's scripts/FPS/MouseLook.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+	public float sensivity = 125f;
+	float xRot = 0f;
+	public Transform body;
+	public bool cursorLock;
+
+
+    void Update()
+    {
+		float h = Input.GetAxis("Mouse X") * sensivity * Time.deltaTime;
+		float v = Input.GetAxis("Mouse Y") * sensivity* Time.deltaTime;
+
+
+		xRot -= v;
+		xRot = Mathf.Clamp(xRot, -90f, 90f);
+
+		transform.localRotation = Quaternion.Euler(xRot, 0f, 0f);
+		body.Rotate(Vector3.up * h);
+
+		if (cursorLock)
+		{
+			Cursor.lockState = CursorLockMode.Locked;
+		}
+		else
+		{
+			Cursor.lockState = CursorLockMode.None;
+		}
+
+	}
+}
