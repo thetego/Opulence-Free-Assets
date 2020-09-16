7615eeebc5ac532c75bb96e5b515b207f8828931
thetego <30575376+thetego@users.noreply.github.com>
2020-09-16 16:23:25 +0300

    DoorSystem

---
+++ Codes (Scripts)/thetego's scripts/Door System/CameraHit.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CameraHit : MonoBehaviour
+{
+
+	public Image cursor;
+
+	private void Update()
+	{
+		RaycastHit hit;
+		if (Physics.Raycast(transform.position,transform.forward,out hit, 5F))
+		{
+			cursor.color = Color.green;
+			if (hit.transform.tag == "Door")
+			{
+				if(Input.GetKeyDown(KeyCode.E))
+					hit.transform.GetComponent<DoorSystem>().IsTheDoorLocked();
+			}
+		}
+		else
+		{
+			cursor.color = Color.white;
+		}
+	}
+}
---
+++ Codes (Scripts)/thetego's scripts/Door System/DoorSystem.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorSystem : MonoBehaviour
+{
+	Quaternion beginnigRot;
+	public bool isLocked, isOpen = false;
+	public float Angle = 90;
+	public float Smooth = 0.05f;
+	
+
+	private void Awake()
+	{
+		beginnigRot = transform.localRotation;
+	}
+
+	public void IsTheDoorLocked()
+	{
+		if (!isLocked)
+		{
+			isOpen = !isOpen;
+		}
+	}
+	private void Update()
+	{
+		Quaternion currentRot = transform.rotation;
+		Quaternion newRot = Quaternion.Euler(transform.eulerAngles.x, Angle, transform.eulerAngles.z);
+
+		if (isOpen)
+			transform.rotation = Quaternion.Lerp(currentRot, newRot,Smooth);
+		else
+			transform.rotation = Quaternion.Lerp(currentRot, beginnigRot, Smooth);
+
+	}
+}
