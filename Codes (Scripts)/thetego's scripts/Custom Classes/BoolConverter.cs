2f8a5e208b33bade5386b05e518e4acdf96327c8
thetego <30575376+thetego@users.noreply.github.com>
2020-12-02 00:30:50 +0300

    BoolConverterClass

---
+++ Codes (Scripts)/thetego's scripts/Custom Classes/BoolConverter.cs
@@ -0,0 +1,24 @@
+﻿//////////////////////////////////////////
+//Written by thetego (@muratcan.thetego)//
+//              Opulence ©              //
+//////////////////////////////////////////
+using UnityEngine;
+public class BoolConverter 
+{
+    public static int BoolToInt (bool value)
+    {
+        if (value)
+            return 1;
+        else
+            return 0;
+    }
+    public static bool IntToBool(int value)
+    {
+        if (value == 1)
+            return true;
+        else if (value == 0)
+            return false;
+        else if (value != 0 && value != 1)
+            Debug.LogError("Error : The input value is not equal to 1 or 0 (So automatically assigned false)"); return false;
+    }
+}
