da9e219f5a4e53ff4df5af883d90731b9dfa45ae
thetego <30575376+thetego@users.noreply.github.com>
2020-12-15 03:38:16 +0300

    Colour Custom Class Update

--- Codes (Scripts)/thetego's scripts/Custom Classes/Colour.cs
+++ Codes (Scripts)/thetego's scripts/Custom Classes/Colour.cs
@@ -12,12 +12,14 @@ using System.IO;
 public class Colour
 {
     #region Variables
-        public string name;
-        public byte r,g,b,a;
-        public static List<Colour> Colours {get;} = new List<Colour>();
+        public string name; //name for a new name to save
+        public byte r,g,b,a; //r,g,b,a values for a new colour to save (0-255)
+        public static List<Colour> Colours {get;} = new List<Colour>(); //list of saved colours
     #endregion
     #region CustomColors
-        public static Color32 red {get;} = new Color32(255,0,0,255);
+        public static Color32 Red {get;} = new Color32(255,0,0,255);
+        public static Color32 Blue {get;} = new Color32(0,0,255,255);
+        public static Color32 Green {get;} = new Color32(0,255,0,255);
         public static Color32 BlueCrayola {get;} = new Color32(33,118,255,255);
         public static Color32 Sunglow {get;} = new Color32(253,202,64,255);
         public static Color32 Orange {get;} = new Color32(247,125,36,255);
@@ -37,15 +39,15 @@ public class Colour
         public static Color32 Turquoise {get;} = new Color32(41,231,205,255);
         public static Color32 PinkDick {get;} = new Color32(224,172,213,255);
     #endregion
-    public static Color32 NewColour (byte r, byte g, byte b, byte a)
+    public static Color32 NewColour (byte r, byte g, byte b, byte a) //creates disposable colour
     {
         Color32 clr = new Color32 (r,g,b,a);
         return clr;
     }
-    public static void LoadColours()
+    public static void LoadColours() //Loads all saved colours
     {
+        //File.Decrypt(Application.dataPath+"/ColourClass"+"/CustomColours.txt");
         string[] lines = File.ReadAllLines(@"Assets/ColourClass/CustomColours.txt");
-
         int lineCount = File.ReadAllLines(@"Assets/ColourClass/CustomColours.txt").Length;
         string[] words;
         foreach (string item in lines)
@@ -59,18 +61,108 @@ public class Colour
             Colours.Add(new Colour(name,r,g,b,a));
         }
     }
+    static int Length; //length of saved colours list
+    public static Color32 GetColour(string ColourName) //calls a saved colour
+    {
+       // File.Decrypt(Application.dataPath+"/ColourClass"+"/CustomColours.txt");
+        Color32 tempColor = new Color32();
+        foreach (Colour item in Colours)
+        {
+            if (item.name == ColourName)
+            {
+                tempColor.r = item.r;
+                tempColor.g = item.g;
+                tempColor.b = item.b;
+                tempColor.a = item.a;
+            }
+            else 
+            {
+                Length++;
+            }
+            if (Length>Colours.Count)
+            {
+                Debug.LogError("There is no declared colour called "+"'"+ColourName+"'"+" in colour list.");
+                Length=0;
+            }
+        }
+        return tempColor;
+    }
+    public static void RemoveColour(string ColourName) //removes a saved colour
+	{
+       
+        foreach (Colour item in Colours)
+        {
+            if (item.name == ColourName)
+            {
+                //File.Decrypt(Application.dataPath+"/ColourClass"+"/CustomColours.txt");
+                Colours.Remove(item);
+                ReListColours();
+                break;
+            }
+            else 
+            {
+                Length++;
+            }
+            if (Length>Colours.Count)
+            {
+                Debug.LogError("There is no declared colour called "+"'"+ColourName+"'"+" in colour list.");
+                Length=0;
+            }
+        }
+        
+	}
+    static void ReListColours () //Refreshes saved colour list
+    {
+        for(int i = 0; i < Colours.Count; i++)
+        {
+            if (i == 0)
+                File.WriteAllText(Application.dataPath+"/ColourClass"+"/CustomColours.txt",Colours[i].name+" "+Colours[i].r.ToString()+" "+Colours[i].g.ToString()+" "+Colours[i].b.ToString()+" "+Colours[i].a.ToString()+"\n");
+            else 
+            {
+                File.AppendAllText(Application.dataPath+"/ColourClass"+"/CustomColours.txt",Colours[i].name+" "+Colours[i].r.ToString()+" "+Colours[i].g.ToString()+" "+Colours[i].b.ToString()+" "+Colours[i].a.ToString()+"\n");
+            }
+        }
+        
+    }
     public static void CreateColour (string name,byte r, byte g, byte b, byte a)
     {
         if(Directory.Exists(Application.dataPath+"/ColourClass"))
         {
+            
             if (File.Exists(Application.dataPath+"/ColourClass"+"/CustomColours.txt"))
             {
-                File.AppendAllText(Application.dataPath+"/ColourClass"+"/CustomColours.txt","\n"+name+" "+r.ToString()+" "+g.ToString()+" "+b.ToString()+" "+a.ToString());
+                Length=0;
+                if (Colours.Count == 0)
+                {
+                    File.AppendAllText(Application.dataPath+"/ColourClass"+"/CustomColours.txt",name+" "+r.ToString()+" "+g.ToString()+" "+b.ToString()+" "+a.ToString()+"\n");
+                    LoadColours();
+                }
+                else 
+                {
+                    foreach (Colour item in Colours)
+                    {
+                        if (name == item.name)
+                        {
+                            Debug.LogError("There is a color called "+"'"+name+"'"+" already.");
+                        }
+                        else 
+                        {
+                            Length++;
+                        }
+                        if (Length>=Colours.Count)
+                        {
+                            Debug.Log("added");
+                            File.AppendAllText(Application.dataPath+"/ColourClass"+"/CustomColours.txt",name+" "+r.ToString()+" "+g.ToString()+" "+b.ToString()+" "+a.ToString()+"\n");
+                            Length=0;
+                        }
+                }
+                }
+
             }
             else 
             {
                 File.Create(Application.dataPath+"/ColourClass"+"/CustomColours.txt");
-                File.AppendAllText(Application.dataPath+"/ColourClass"+"/CustomColours.txt",name+" "+r.ToString()+" "+g.ToString()+" "+b.ToString()+" "+a.ToString());
+                File.AppendAllText(Application.dataPath+"/ColourClass"+"/CustomColours.txt",name+" "+r.ToString()+" "+g.ToString()+" "+b.ToString()+" "+a.ToString());;
             }
         }
         else 
@@ -79,6 +171,11 @@ public class Colour
             File.Create(Application.dataPath+"/ColourClass"+"/CustomColours.txt");
             File.AppendAllText(Application.dataPath+"/ColourClass"+"/CustomColours.txt",name+" "+r.ToString()+" "+g.ToString()+" "+b.ToString()+" "+a.ToString());
         }
+    }
+    public static void Reset ()
+    {
+        File.Delete(Application.dataPath+"/ColourClass"+"/CustomColours.txt");
+        Colours.Clear();
     }
     public Colour (string _name, byte _r,byte _g, byte _b, byte _a)
     {
